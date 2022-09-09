560f67b0fce47f79a7ffea2aa6ad4318f9165b94
Jaewook Kim <jaewkim@c2r3s8.42seoul.kr>
2022-09-09 18:49:48 +0900

    add test build  in jay_irc

---
+++ jay_irc/inc/main.h
@@ -0,0 +1,16 @@
+//
+// Created by Jaewook Kim on 9/9/22.
+//
+
+#ifndef FT_IRC_MAIN_H
+#define FT_IRC_MAIN_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+
+
+#endif //FT_IRC_MAIN_H
