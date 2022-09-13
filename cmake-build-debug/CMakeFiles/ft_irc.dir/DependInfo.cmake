c60c0859faa6168ccfd92bdb5a8377fe8d4e83bc
Jaewook Kim <jaewkim@c3r5s5.42seoul.kr>
2022-09-13 19:18:52 +0900

    asdf

---
+++ cmake-build-debug/CMakeFiles/ft_irc.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/goinfre/jaewkim/ft_irc/jay_irc/src/main.cpp" "CMakeFiles/ft_irc.dir/jay_irc/src/main.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/jay_irc/src/main.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/Channel.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Channel.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Channel.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/Client.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Client.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Client.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/Message.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Message.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Message.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/Server.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Server.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/Server.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/commands.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/commands.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/commands.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/libft.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/libft.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/libft.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/main.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/main.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/main.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/ssl.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/ssl.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/ssl.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/IRC_server/src/utils.cpp" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/utils.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/IRC_server/src/utils.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/channel.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/channel.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/channel.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/config.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/config.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/config.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/main.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/main.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/main.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/mainframe.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/mainframe.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/mainframe.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/parser.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/parser.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/parser.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/server.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/server.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/server.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/session.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/session.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/session.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/ref_git/simple-irc-server/user.cpp" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/user.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/ref_git/simple-irc-server/user.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/select.cpp" "CMakeFiles/ft_irc.dir/select.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/select.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/test.cpp" "CMakeFiles/ft_irc.dir/test.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/test.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/webserv/client.cpp" "CMakeFiles/ft_irc.dir/webserv/client.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/webserv/client.cpp.o.d"
+  "/goinfre/jaewkim/ft_irc/webserv/server.cpp" "CMakeFiles/ft_irc.dir/webserv/server.cpp.o" "gcc" "CMakeFiles/ft_irc.dir/webserv/server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
