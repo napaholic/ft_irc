83c8a926f47323cdbbafef172bf061e96de42484
Naaam <skagudtlr731@gmail.com>
2022-09-16 12:43:50 +0900

    server

--- jay_irc/src/main.cpp
+++ jay_irc/src/main.cpp
@@ -4,8 +4,18 @@
 
 #include "../inc/main.h"
 
-
 int main(int argc, char **argv)
 {
-
+    try
+    {
+        if (argc != 3)
+            throw std::invalid_argument("please ./ircserv port password");
+        
+        Server server(argv[1], argv[2]);
+        server.run();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
