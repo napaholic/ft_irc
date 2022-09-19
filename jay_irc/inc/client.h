80d0d2bb114b8aad770e35303f1ca1781de0fd00
Yunhui Bae <yubae@student.42seoul.kr>
2022-09-19 18:28:03 +0900

    addr

--- jay_irc/inc/client.h
+++ jay_irc/inc/client.h
@@ -5,20 +5,28 @@
 #ifndef FT_IRC_CLIENT_H
 #define FT_IRC_CLIENT_H
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string>
+
 class Client {
 public:
-    Client(int socket);
+    Client(int socket, sockaddr_in client_addr);
     ~Client();
     Client(const Client &rhs);
     int getSocket() const;
+    void setClient();
 
 private:
     int __socket;
+    struct sockaddr_in __client_addr;
     std::string __nickname;
     std::string __username;
     std::string __hostname;
     std::string __servername;
     std::string __realname;
+
+    std::string __prefix;
 
     char buf[512];
 
--- jay_irc/src/client.cpp
+++ jay_irc/src/client.cpp
@@ -5,8 +5,8 @@
 #include "../inc/client.h"
 
 
-Client::Client(int socket)
-    :__socket(socket)
+Client::Client(int socket, struct sockaddr_in client_addr)
+    :__socket(socket), __client_addr(client_addr)
 {
     memset(buf, 0, sizeof(buf));
     std::cout << "Unknown client" << socket << "created.\n";
@@ -18,9 +18,20 @@ Client::~Client()
 }
 
 Client::Client(const Client &rhs)
-    :__socket(rhs.socket)
+    :__socket(rhs.__socket)
 {
     memset(buf, 0, sizeof(buf));
+}
+
+void Client::setClient()
+{
+    __prefix.clear();
+    __prefix = std::string(":").append(__nickname);
+    __prefix.append("!");
+    __prefix.append(__username);
+    __prefix.append("@");
+    std::string ip = std::to_string(__client_addr.sin_port);
+    __prefix.append(ip);
 }
 
 int Client::getSocket() const
